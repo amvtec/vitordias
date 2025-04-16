90b3eace2d3feacb78d9e106e4bc0aac7e5f7a34
Alexandre Martins Vieira <alx.techto@gmail.com>
2025-04-16 20:09:51 -0300

    Alteracoes nos settings

--- funcionarios/templatetags/custom_filters.py
+++ funcionarios/templatetags/custom_filters.py
@@ -8,3 +8,10 @@ def primeiros_dois_nomes(nome):
         return ''
     partes = nome.split()
     return ' '.join(partes[:2]) if len(partes) >= 2 else nome
+
+@register.filter
+def somar_horas(valor1, valor2):
+    try:
+        return float(valor1) + float(valor2)
+    except (TypeError, ValueError):
+        return valor1 or 0
