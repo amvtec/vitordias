07a8fcb91b733b39a4a31736df71a84d6814961d
Alexandre Martins Vieira <alx.techto@gmail.com>
2025-04-16 22:32:57 -0300

    Corrigido ALLOWED_HOSTS para Render

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-str=d@-**sv13#x&ztx@h^y#mvj_g&(^&m(e)q!$*hbj)g!dr%'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
