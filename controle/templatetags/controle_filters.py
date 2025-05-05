2afa79a8f7c63055faede038b0c1b7305e83c72a
Alexandre Martins Vieira <alx.techto@gmail.com>
2025-05-05 20:44:50 -0300

    Alteracoes nos views

--- controle/templatetags/controle_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def abreviar_dia_semana(value):
-    if value:
-        return value.replace('-feira', '').strip()
-    return value
