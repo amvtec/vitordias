c29dc325ef3dbdcf6abe6482ab2da20da8a7cb1f
Alexandre Martins Vieira <alx.techto@gmail.com>
2025-05-06 22:51:49 -0300

    Alteracoes nos templates

--- controle/templatetags/controle_filters.py
+++ controle/templatetags/controle_filters.py
@@ -7,3 +7,14 @@ def abreviar_dia_semana(value):
     if value:
         return value.replace('-feira', '').strip()
     return value
+
+MESES_PT_BR = {
+    1: "Janeiro", 2: "Fevereiro", 3: "Março", 4: "Abril",
+    5: "Maio", 6: "Junho", 7: "Julho", 8: "Agosto",
+    9: "Setembro", 10: "Outubro", 11: "Novembro", 12: "Dezembro"
+}
+
+@register.filter
+def mes_extenso(numero_mes):
+    return MESES_PT_BR.get(numero_mes, "")
+
